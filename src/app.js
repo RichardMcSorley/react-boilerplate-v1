9e60df2ab18c178e10708e82fb2af08330cf4f7b
r0m00n5 <richard.mcsorley@walmart.com>
2017-11-04 13:33:44 -0500

    init

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import Root from './components/Root';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+import ReactDOM  from 'react-dom';
+
+ReactDOM.render(<Root />, document.getElementById('app'))
---
+++ src/components/Root.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Root extends Component {
+    componentDidMount() {
+    }
+    componentDidUpdate(prevProps, prevState) {
+        
+    }
+    componentWillUnmount() {
+    }
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
